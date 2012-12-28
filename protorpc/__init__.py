cc4ba2b3a39fd8f7ce39a3e5e037bb1432e1f06b
Gregory Nicholas <faction.gregory@gmail.com>
2012-12-28 00:15:25 -0800

    added declare_namespace() to protorpc/__init__.py to fix warning from setup.py install

--- protorpc/__init__.py
+++ protorpc/__init__.py
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+__import__('pkg_resources').declare_namespace(__name__)
 
 """Main module for ProtoRPC package."""
 
